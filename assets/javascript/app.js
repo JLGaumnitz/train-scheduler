b25fb2a96ced12b4a849c16252d9c82d524b0b2b
JLG <mjgaumnitz@kc.rr.com>
2019-10-17 01:39:05 -0500

    reworked some CSS, tried to get calculations to work/did not work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,69 +13,69 @@ firebase.initializeApp(firebaseConfig);
 var dataRef = firebase.database();
 
 // Initial Values
-var trainNumber = "";
+var trainName = "";
 var trainDestination = "";
-var firstTrainTime = "";
-var trainFrequency = "";
+var firstTrainTime = "00:00";
+var trainFrequency = "0";
 
 //   On click function to add trains to the top table
 $("#add-train-btn").on("click", function (event) {
   event.preventDefault();
   
   // Variables with the user input from the bottom form
-  trainNumber = $("#formTrainNumber").val().trim();
+  trainName = $("#formTrainName").val().trim();
   trainDestination = $("#formDestination").val().trim();
   firstTrainTime = $("#formFirstTime").val().trim();
   trainFrequency = $("#formFrequency").val().trim();
 
-  // Code for the push to Firebase
+  // Code to push to Firebase
   dataRef.ref().push({
-    number: trainNumber,
+    number: trainName,
     destination: trainDestination,
     firstTime: firstTrainTime,
     frequency: trainFrequency,
   });
 });
 
-// Calculation of the Next Arrival and Minutes to Arrival fields
+// Calculation of the Next Arrival and Minutes Away
 
 // Ensure the first train time is after the current time.
-var convertedFirstTrainTime = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+var convertedFirstTrainTime = moment(firstTrainTime, "hh:mm a").subtract(1, "years");
 
 // Store variable for current time.
-var currentTime = moment().format("HH:mm");
-console.log("Current time is:" + currentTime);
+var currentTime = moment().format("HH:mm a");
 
 // Calculate difference between current time and first train time.
-var elapsedTimefromFirstTrain = moment().diff(moment(convertedFirstTrainTime), "minutes");
+var elapsedTimeSinceFirstTrain = moment().diff(moment(convertedFirstTrainTime), "minutes");
 
 // Calculate the time left (modulus of elapsed time divided by train frequency).
-var timeLeft = elapsedTimefromFirstTrain % trainFrequency;
+var timeLeft = elapsedTimeSinceFirstTrain % trainFrequency;
 
 // Calculate and store the minutes until next train arrives.
 var minutesAway = trainFrequency - timeLeft;
 
-// Calculate the next arriving train
-var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm A");
+// Calculate the next arriving train.
+var nextArrival = moment().add(minutesAway, "minutes").format("h:mm a");
 
-// Firebase watcher + initial loader (This code behaves similarly to .on("value"))
+
+// Firebase watcher + initial loader (Per lesson: This code behaves similarly to .on("value"))
 dataRef.ref().on("child_added", function(childSnapshot) {
 
-  // Log everything that is coming out of childSnapshot
+  // Log items coming out of childSnapshot.
   console.log(childSnapshot.val().number);
   console.log(childSnapshot.val().destination);
   console.log(childSnapshot.val().firstTime);
   console.log(childSnapshot.val().frequency);
   
-  // Add the data into the table in HTML
+  // Add the data to the table in HTML.
   $("#trainScheduleBody").append("<tr><td>" + childSnapshot.val().number + "</td><td>" + childSnapshot.val().destination + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
 },
   function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
   });
 
-  // Clear the bottom form's values
-  $("#formTrainNumber").val("");
+  // Clear the bottom form's values.
+  $("#formTrainName").val("");
   $("#formDestination").val("");
   $("#formFirstTime").val("");
   $("#formFrequency").val("");
